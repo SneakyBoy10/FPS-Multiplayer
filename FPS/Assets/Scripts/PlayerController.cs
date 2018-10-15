c947082f548e724a5cce49b75a9a7b8e607ec7fc
LAPTOP-GV137KLR\tomal <tomalfieb10@gmail.com>
2018-10-15 18:38:44 +0100

    Basics

---
+++ FPS/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float looksens = 3f;
+
+    private PlayerMotor motor;
+
+    private void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    private void Update()
+    {
+        float _xMov = Input.GetAxisRaw("Horizontal");
+        float _zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 _movHor = transform.right * _xMov;
+        Vector3 _movVer = transform.forward * _zMov;
+
+        Vector3 _velocity = (_movHor + _movVer).normalized * speed;
+
+        motor.Move(_velocity);
+
+        float _yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0f, _yRot, 0) * looksens;
+
+        motor.Rotate(_rotation);
+
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 _cameraRotation = new Vector3(_xRot, 0, 0) * looksens;
+
+        motor.RotateCamera(_cameraRotation);
+    }
+}
